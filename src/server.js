6ab153028a1d07d3e1390197cd69a5f14da4a0b7
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-05-07 15:13:54 -0300

    Adicionado Procfile

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app')
 
-const port = process.env.APP_PORT || 3333
+const port = process.env.PORT || 3333
 
 module.exports = app.listen(port, () => { console.log(`Servidor rodando na porta ${port}`) })

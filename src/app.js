02e4e8266dc55be1e3cbd0c8cd9b25f0c159235c
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-05-07 14:53:08 -0300

    Commit inicial

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const Express = require('express')
+const bodyParser = require('body-parser')
+
+const routes = require('./routes')
+
+require('./database')
+
+class App {
+  constructor () {
+    this.server = new Express()
+
+    this.middlewares()
+    this.routes()
+  }
+
+  middlewares () {
+    this.server.use(bodyParser.urlencoded({ extended: true }))
+    this.server.use(Express.json())
+  }
+
+  routes () {
+    this.server.use(routes)
+  }
+}
+
+module.exports = new App().server
